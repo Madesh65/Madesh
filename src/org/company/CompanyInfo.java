c4939ca75d027e2b236f306f731fca1f0f8e855a
Madesh65 <madeshgysm7@gmail.com>
2021-11-13 07:50:35 +0530

    Project added

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,18 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName()
+	{
+		System.out.println("Company name is: Wipro");
+	}
+    private void companyId()
+    {
+    	System.out.println("Company Id is:w230");
+    }
+    private void companyAddress()
+    {
+    	System.out.println("Company Address is: Chennai");
+    }
+   
+
+}
